b11c84d706419ada953f3effa0013bfc2e81bc4c
Nick-Hericks <nickhericks@gmail.com>
2018-12-28 13:57:35 +0200

    Add search filtering

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
-// Study guide for this project
-// https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
+/* GLOBAL VARIABLES */
 const page = document.querySelector('.page'); // used for appendPageLinks function
-const studentItems = document.querySelectorAll('.student-item');
+const fullStudentList = document.querySelectorAll('.student-item');
+let searchString = '';
+let filteredStudentList = [];
 
-let selectedButton = 1;
 
-// Use JS to create and append search to the page
+// Append search bar feature to the page
 const pageHeader = document.querySelector('.page-header');
 const searchDiv = document.createElement('div');
 searchDiv.className = 'student-search';
@@ -14,70 +13,94 @@ searchDiv.innerHTML = `
   <input placeholder="Search for students...">
   <button>Search</button>
 `;
+
+searchDiv.addEventListener('input', (e) => {
+  // console.log(event.target.value);
+
+  searchString = event.target.value;
+  search();
+});
+
 pageHeader.appendChild(searchDiv);
 
 
+// Create elements for pagination buttons
+const linksDiv = document.createElement('div');  // Create div element
+const linksUl = document.createElement('ul');    // Create ul element
+linksDiv.className = 'pagination';               // Give class to div
+linksDiv.appendChild(linksUl);                   // Append ul to div
+
+// Create elements to display if no students match search query
+// TODO: When a search yields 0 results, a message is displayed on the page, informing the user that no results have been found.
+
+const noResults = document.createElement('div');
+noResults.textContent = `Sorry, no students match your search query.`;
+noResults.style.display = 'none';
+page.appendChild(noResults);
+
+
+
 /***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
+   showPage function
+   Hide all items in array except for the ten you want to show
+   INPUT:  HTMLCollection (array) of student list items
+   OUTPUT: Changes CSS of student list items, returns nothing
 ***/
-
 const showPage = (list, button) => {
   // Reset page to hide all students
-  for(let i = 0; i < list.length; i++) {
-    list[i].style.display = 'none';
+  for(let i = 0; i < fullStudentList.length; i++) {
+    fullStudentList[i].style.display = 'none';
   }
+  // Reset page to hide all students
+  // for(let i = 0; i < list.length; i++) {
+  //   list[i].style.display = 'none';
+  // }
 
-  let listStart = ((button - 1) * 10);
-  let listEnd = (listStart + 10);
-  console.log(listStart);
-  console.log(listEnd);
-  console.log(list.length);
+  if(list.length === 0) {
+    console.log("none to show!!!");
+    noResults.style.display = 'block';
+  } else {
+    noResults.style.display = 'none';
 
-  // Display the block of students requested
-  for(let i = listStart; i < listEnd; i++) {
-    list[i].style.display = 'block';
+    let listStart = ((button - 1) * 10);
+    let listEnd = (listStart + 10);
+    // console.log(listStart);
+    // console.log(listEnd);
+    console.log(list.length);
+
+    // Display the block of students requested
+    for(let i = listStart; i < listEnd; i++) {
+      list[i].style.display = 'block';
+    }
   }
-
-
 
 }
-showPage(studentItems, selectedButton);
-
-
 
 /***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
+   appendPageLinks function
+   Generate, append and add functionality to pagination buttons
+   INPUT:
+   OUTPUT: , returns nothing
 ***/
-
 function appendPageLinks(studentList) {
-  // Create elements
-  const linksDiv = document.createElement('div');  // Create div element
-  const linksUl = document.createElement('ul');    // Create ul element
-  linksDiv.className = 'pagination';               // Give class to div
-  linksDiv.appendChild(linksUl);                   // Append ul to div
+  let selectedButton = 1;
+
+  showPage(studentList, selectedButton);
+
+  linksUl.innerHTML = '';
+
+
 
   let numPages = 0;                                // Reset numPages
 
   // Find total number of students
   let numStudents = studentList.length;
-  // console.log(`Number of students is: ${numStudents}`);
   console.log(`numStudents: ${numStudents}`);
 
 
   for(let i = 0; i < numStudents; i += 10) {
     numPages++;
   }
-  // console.log(`Number of pages: ${numPages}  (${numStudents}/10 = ${numPages -1} + 1 for remaining students)`);
   console.log(`numPages: ${numPages}`);
 
 
@@ -93,8 +116,6 @@ function appendPageLinks(studentList) {
     anchor.textContent = i + 1;
     button.appendChild(anchor);
     linksUl.appendChild(button);
-
-    //console.log(button);
   }
 
 
@@ -112,9 +133,31 @@ function appendPageLinks(studentList) {
     showPage(studentList, selectedButton);
   });
 
-
-
   page.appendChild(linksDiv);
 }
+// Call when file is initially loaded
+appendPageLinks(fullStudentList);
 
-appendPageLinks(studentItems);
+
+
+
+const search = () => {
+  filteredStudentList = [];
+
+  if(searchString.length === 0) {
+    appendPageLinks(fullStudentList);
+  } else {
+    // For each item in the full student list
+    for(let i = 0; i < fullStudentList.length; i++) {
+      // Select the student's name as a string
+      let name = fullStudentList[i].querySelector('h3').textContent;
+      // If the student's name contains the search query substring
+      if(name.includes(searchString)) {
+        // Add student item HTML to filteredStudentList
+        filteredStudentList.push(fullStudentList[i]);
+        // console.log(filteredStudentList);
+      }
+    }
+    appendPageLinks(filteredStudentList);
+  }
+}
